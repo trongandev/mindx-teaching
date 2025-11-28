4104b974f5a3e698ab725281814955a985ec6c6e
Trong An <troandev@gmail.com>
2025-11-28 13:47:44 +0700

    upload

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,5 +1,11 @@
 import { Outlet } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function AuthLayout() {
-    return <Outlet />
+    return (
+        <>
+            <Outlet />
+            <Analytics />
+        </>
+    )
 }
